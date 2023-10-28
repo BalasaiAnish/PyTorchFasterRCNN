67f4b1d3fc680b3fff22970baa2f89448d60e5b9
Balasai Ponnaluri <148792200+BalasaiAnish@users.noreply.github.com>
2023-10-28 18:45:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ from torchvision.utils import draw_bounding_boxes
 #Importing OpenCV to capture and process images from the webcam
 import cv2
 
+#Importing time for FPS calculation
+import time
+
 #Defining custom fucntions for modularity and clarity of code
+
 #Function that takes the video capture object from VideoCapture and an image size and returns a tensor that can be fed into the model
 def takePicture(cap,device,size=(224,224)):
 	success,image=cap.read()
@@ -51,17 +55,21 @@ model=fasterrcnn_resnet50_fpn(weights=weights)
 model.eval()
 model.to(device)
 
+#Setting variables for FPS calclation
+current_time=0
+prev_time=0
+
 #Opening the webcam to start taking video
 cap=cv2.VideoCapture(0)
 
-if not cap.IsOpened:
+if not cap.isOpened:
 	print('Unable to open wecam, please check your settings')
 #Infinite loop to keep running unitl the esc key is pressed
 while True:
-  #Takes an image from the wecam and return a tensor suitable as an inout for the model
-	image=takePicture(cap,,device,size=(224,224))
+	#Takes an image from the wecam and return a tensor suitable as an inout for the model
+	image=takePicture(cap,device,size=(224,224))
 
-  #Running the image throught the neural network
+	#Running the image throught the neural network
 	output=model(image)[0]
 
 	#Calculating FPS using 1/Processing_time formula
@@ -79,21 +87,23 @@ while True:
 	
 	#Setting a font
 	font=cv2.FONT_HERSHEY_SIMPLEX 
+
+	#Take the image and output of the model as inputs and return an image with bounding boxes and labels drawn on it
+	output_image=drawBoundingBoxes(image,output)
 	
 	#Placing the value for fps on to the screen
-	cv2.putText(img=output_image, text=fps, org=(5,224-5), fontFace=font, fontScale=0.5, color=(0, 0, 255),thickness=1)
+	cv2.putText(img=output_image, text=fps, org=(5,224-5), fontFace=font, fontScale=0.375, color=(0, 0, 255),thickness=1)
 
-  	#Take the image and output of the model as inputs and return an image with bounding boxes and labels drawn on it
-	output_image=drawBoundingBoxes(image,output)
-  
-
-
-  #Displaying new image with labels and bounding boxes
+	#Displaying new image with labels and bounding boxes
 	cv2.imshow('FasterRCNN',output_image)
 
-  #Exits the loop if esc key is pressed
-  if cv2.waitKey(100)==27:
+	#Exits the loop if esc key is pressed
+	if cv2.waitKey(1)==27:
 		break
+
+#Closing the webcam and destroying all created windows	
+cap.release()
+cv2.destroyAllWindows()
 
 #Closing webcam and all created windows
 cap.release()
